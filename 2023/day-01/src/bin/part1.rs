13e0223ea03840237d49c677f59b12c44ac52466
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2023-12-02 21:11:11 -0700

    completed day 1 part 1

--- 2023/day-01/src/bin/part1.rs
+++ 2023/day-01/src/bin/part1.rs
@@ -1,11 +1,25 @@
 fn main() {
-    let input = include_str!("./input1.txt");
+    let input = include_str!("input.txt");
     let result = part1(input);
     dbg!(result);
 }
 
-fn part1(input: &str) -> String {
-    "todo!".to_string()
+fn part1(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .filter_map(|char| char.to_digit(10))
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<Vec<u32>>>()
+        .iter()
+        .map(|arr| {
+            let first = arr[0] * 10;
+            let last = arr[arr.len() - 1];
+            first + last
+        })
+        .sum::<u32>()
 }
 
 #[cfg(test)]
@@ -13,8 +27,19 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = part1("");
-        assert_eq!(result, "todo!".to_string());
+    fn check_example() {
+        let input = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+        let result = part1(input);
+        assert_eq!(result, 142);
+    }
+
+    #[test]
+    fn check_answer() {
+        let input = include_str!("input.txt");
+        let result = part1(input);
+        assert_eq!(result, 55123);
     }
 }
--- 2023/day-01/src/bin/part2.rs
+++ 2023/day-01/src/bin/part2.rs
@@ -1,11 +1,41 @@
+use regex::Regex;
+
 fn main() {
-    let input = include_str!("./input2.txt");
+    let input = include_str!("input.txt");
     let result = part2(input);
     dbg!(result);
 }
 
-fn part2(input: &str) -> String {
-    "todo!".to_string()
+fn part2(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let regex = Regex::new(r"[1-9]|one|two|three|four|five|six|seven|eight|nine").unwrap();
+
+            regex
+                .find_iter(line)
+                .map(|m| match m.as_str() {
+                    "one" => 1,
+                    "two" => 2,
+                    "three" => 3,
+                    "four" => 4,
+                    "five" => 5,
+                    "six" => 6,
+                    "seven" => 7,
+                    "eight" => 8,
+                    "nine" => 9,
+                    _ => m.as_str().parse::<u32>().unwrap(),
+                })
+                .collect::<Vec<u32>>()
+        })
+        .collect::<Vec<Vec<u32>>>()
+        .iter()
+        .map(|arr| {
+            let first = arr[0] * 10;
+            let last = arr[arr.len() - 1];
+            first + last
+        })
+        .sum::<u32>()
 }
 
 #[cfg(test)]
@@ -13,8 +43,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = part2("");
-        assert_eq!(result, "todo!".to_string());
+    fn check_example() {
+        let input = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+        let result = part2(input);
+        assert_eq!(result, 281);
+    }
+
+    #[test]
+    fn check_answer() {
+        let input = include_str!("input.txt");
+        let result = part2(input);
+        assert_eq!(result, 0);
     }
 }
